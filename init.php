5e887a0fa095a2fcaad250e6d1cace47be1b5c96
Rohit Shigvan <67287137+TARS-64bit@users.noreply.github.com>
2022-05-15 01:16:23 +0530

    Add files via upload

---
+++ init.php
@@ -0,0 +1,8 @@
+<?php
+	$servername = "id18931085_srms";
+	$username = "id18931085_rohit";
+	$password = "Xs)Xh=@P/{&SHc~9";
+	$dbhost = "localhost";
+	$conn = mysqli_connect($dbhost, $username, $password, $servername);
+	$db = mysqli_select_db($conn,'id18931085_srms');
+?>
